cac9c2d0a53f8a5f41e0cdbe038ad00b6acef9be
ChandanaRD <chanrd1996@gmail.com>
2019-07-03 13:17:08 +0530

    refactoring to class component, conditional rendering, use of lifecycle method, gh-pages start

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const SeasonDisplay = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => { console.log(position) },
-        (err) => { console.log(`err: ${err}`) }
-    );
+    
 return <div>Season Display!</div>
 }
 
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,18 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { lat:null, errorMessage: '' };
+    }
 
-const App = () => {
+    componentDidUpdate(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { this.setState({lat:position.coords.latitude}) },
+            (err) => { this.setState({errorMessage:err.message}) }
+        );
+        console.log(`lat ${this.state.lat} error ${this.state.errorMessage}`)
+    }
 
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => { console.log(position) },
-        (err) => { console.log(err) }
-    );
-
-    return <div> It's working!</div>;
-};
-
+    render(){
+        if(this.state.lat && !this.state.errorMessage){
+            return <div>Latitude: {this.state.lat}</div>;
+        }
+        if(!this.state.lat && this.state.errorMessage){
+            return <div>err: {this.state.errorMessage}</div>;
+        } 
+        if(!this.state.lat && !this.state.errorMessage){
+            return <div>loading!</div>;
+        }
+    }
+}
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+    <App />, document.querySelector('#root')
 )
